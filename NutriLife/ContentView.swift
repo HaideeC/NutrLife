c620a7ac0615aa66332dc611df2e4073b1a0aec9
Haidee <haidee.cxf@gmail.com>
2025-12-09 13:42:04 +0800

    增加Xcode项目工程文件

---
+++ NutriLife/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NutriLife
+//
+//  Created by Haidee on 2025/12/8.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NutriLifeTests/NutriLifeTests.swift
@@ -0,0 +1,17 @@
+//
+//  NutriLifeTests.swift
+//  NutriLifeTests
+//
+//  Created by Haidee on 2025/12/8.
+//
+
+import Testing
+@testable import NutriLife
+
+struct NutriLifeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
